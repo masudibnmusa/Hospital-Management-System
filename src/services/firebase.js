dfd71fd08374f200059eebd0229b98dd16d6a2f9
Masud Ibn Musa <masudibnmusa10@gmail.com>
2026-08-17 05:24:45 +0600

    Adding  src/services/firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAqnAmxing5sXxWhmcdVPlLtS9eWC9UAjU",
+  authDomain: "hospital-management-8c6d5.firebaseapp.com",
+  projectId: "hospital-management-8c6d5",
+  storageBucket: "hospital-management-8c6d5.firebasestorage.app",
+  messagingSenderId: "985715239213",
+  appId: "1:985715239213:web:6cec3c4c4a315103804ba4"
 };
 
 const app = initializeApp(firebaseConfig);
