b1bd0dcf9d247ac7556d65f3771689f714d966e5
Masud Ibn Musa <masudibnmusa10@gmail.com>
2026-08-17 04:00:52 +0600

    adding src/components/common/Layout.jsx

---
+++ src/components/common/Layout.jsx
@@ -0,0 +1,19 @@
+import { Outlet } from 'react-router-dom';
+import Sidebar from './Sidebar.jsx';
+import Navbar from './Navbar.jsx';
+
+const Layout = () => {
+  return (
+    <div className="flex h-screen bg-gray-50 dark:bg-gray-900">
+      <Sidebar />
+      <div className="flex-1 flex flex-col overflow-hidden">
+        <Navbar />
+        <main className="flex-1 overflow-y-auto p-6">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
